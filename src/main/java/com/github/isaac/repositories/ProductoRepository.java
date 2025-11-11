965608992c0d0ab39c9b9ce8219027bd40bcd971
O-Isaac <zaragozamendozaisaac@outlook.es>
2025-11-12 00:57:04 +0100

    Feat: Add method to fetch productos with low stock based on minimum threshold

--- src/main/java/com/github/isaac/repositories/ProductoRepository.java
+++ src/main/java/com/github/isaac/repositories/ProductoRepository.java
@@ -2,9 +2,21 @@ package com.github.isaac.repositories;
 
 import com.github.isaac.entities.Producto;
 import com.github.isaac.repositories.base.BaseRepositoryImpl;
+import com.github.isaac.utils.JPAUtil;
+
+import java.util.List;
+
 
 public class ProductoRepository extends BaseRepositoryImpl<Producto, Long> {
     public ProductoRepository() {
         super(Producto.class);
+    }
+
+    public List<Producto> obtenerConStockBajo(int stockMinimo) {
+        try (var em = JPAUtil.getEntityManager()) {
+            return em.createQuery("SELECT p FROM Producto p WHERE p.stock < :stockMinimo", Producto.class)
+                    .setParameter("stockMinimo", stockMinimo)
+                    .getResultList();
+        }
     }
 }
