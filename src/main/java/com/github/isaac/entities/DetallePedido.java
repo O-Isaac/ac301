2a0955e807d6921e5df972166d7a4d1b63d7d0e3
O-Isaac <zaragozamendozaisaac@outlook.es>
2025-11-15 10:06:08 +0100

    Feat: Add Lombok annotations for builder pattern and constructors in DetallePedido

--- src/main/java/com/github/isaac/entities/DetallePedido.java
+++ src/main/java/com/github/isaac/entities/DetallePedido.java
@@ -2,9 +2,7 @@ package com.github.isaac.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +11,9 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "detalles_pedido")
 @ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetallePedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
